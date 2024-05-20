0681c3f11d530d4f0ff9c1feaa3babd5561ddd0b
Zunayed <zunayedology@gmail.com>
2024-05-20 13:07:51 +0600

    Initial setup completed (next, convex, clerk, shadcn-ui)

--- convex/files.ts
+++ convex/files.ts
@@ -1,4 +1,4 @@
-import { v } from "convex/values";
+import { ConvexError, v } from "convex/values";
 import { mutation, query } from "./_generated/server";
 
 export const createFile = mutation({
@@ -6,6 +6,10 @@ export const createFile = mutation({
     name: v.string(),
   },
   async handler(ctx, args) {
+    const identity = await ctx.auth.getUserIdentity();
+    if (!identity) {
+      throw new ConvexError("You musy be logged in");
+    }
     await ctx.db.insert("files", {
       name: args.name,
     });
@@ -15,6 +19,10 @@ export const createFile = mutation({
 export const getFiles = query({
   args: {},
   async handler(ctx, args) {
+    const identity = await ctx.auth.getUserIdentity();
+    if (!identity) {
+      return [];
+    }
     return ctx.db.query("files").collect();
   },
 });
